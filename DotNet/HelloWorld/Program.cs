6683eaa51d29e0d3e22424a45a536def840125f3
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-02 11:38:59 +0200

    Added C# example application.

---
+++ DotNet/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+DateTime now = DateTime.Today;
+DateTime endOfYear = new(now.Year, 12, 31);
+var diff = endOfYear - now;
+Console.WriteLine(diff.TotalDays);
