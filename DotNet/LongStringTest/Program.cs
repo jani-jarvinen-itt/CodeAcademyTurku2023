d1a7031f48cf41b5b8248d669a5cb63de60bee6a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-07 15:22:44 +0200

    Added string manipulation example with C#.

---
+++ DotNet/LongStringTest/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Starting string generation loop.");
+
+DateTime start = DateTime.Now;
+string s = "";
+for (int i = 0; i < 500_000; i++)
+{
+    s += "A";
+}
+DateTime end = DateTime.Now;
+
+TimeSpan timeTaken = end - start;
+Console.WriteLine("Loop took " + timeTaken.TotalSeconds + " seconds.");
+
+Console.WriteLine("Length of generated string: " + s.Length);
+Console.WriteLine("Completed string generation loop.");
