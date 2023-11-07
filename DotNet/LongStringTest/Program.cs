39f0ccb1e60fa412b6021e75935f78174a1522d7
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-07 15:30:24 +0200

    Changed the string looping to use a StringBuilder instead.

--- DotNet/LongStringTest/Program.cs
+++ DotNet/LongStringTest/Program.cs
@@ -1,15 +1,17 @@
-﻿Console.WriteLine("Starting string generation loop.");
+﻿using System.Text;
+
+Console.WriteLine("Starting string generation loop.");
 
 DateTime start = DateTime.Now;
-string s = "";
+StringBuilder sb = new();
 for (int i = 0; i < 500_000; i++)
 {
-    s += "A";
+    sb.Append("A");
 }
 DateTime end = DateTime.Now;
 
 TimeSpan timeTaken = end - start;
 Console.WriteLine("Loop took " + timeTaken.TotalSeconds + " seconds.");
 
-Console.WriteLine("Length of generated string: " + s.Length);
+Console.WriteLine("Length of generated string: " + sb.Length);
 Console.WriteLine("Completed string generation loop.");
