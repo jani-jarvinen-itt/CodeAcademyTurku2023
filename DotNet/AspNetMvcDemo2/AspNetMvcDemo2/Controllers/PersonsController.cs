c8f72ed7aba97cb13f1c7e999f682c11ead94fdb
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-08 14:55:12 +0200

    Added an example of reading a CSV file and displaying that information inside a HTML table in a view.

---
+++ DotNet/AspNetMvcDemo2/AspNetMvcDemo2/Controllers/PersonsController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetMvcDemo2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcDemo2.Controllers
+{
+    public class PersonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // create a new empty list of PersonModel objects
+            var persons = new List<PersonModel>();
+
+            // read a CSV file from disk and fill the persons list with the data from the CSV file
+            using (var reader = new StreamReader(System.IO.File.OpenRead(@"Persons.csv")))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    // if this is the first line, skip it
+                    if (line.StartsWith("PersonName,YearOfBirth,CountryOfOrigin"))
+                    {
+                        continue;
+                    }
+
+                    var values = line.Split(',');
+
+                    var person = new PersonModel
+                    {
+                        PersonName = values[0],
+                        YearOfBirth = values[1],
+                        Country = values[2]
+                    };
+
+                    persons.Add(person);
+                }
+            }
+
+            return View(persons);
+        }
+    }
+}
---
+++ DotNet/AspNetMvcDemo2/AspNetMvcDemo2/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetMvcDemo2.Models
+{
+    public class PersonModel
+    {
+        public string PersonName { get; set; }
+
+        public string YearOfBirth { get; set; }
+
+        public string Country { get; set; }
+    }
+}
