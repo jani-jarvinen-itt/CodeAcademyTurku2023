0b8d59a61985a1583b156b7e06888acc757ddacf
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-15 14:50:39 +0200

    Added a C# demo on variable equality.

---
+++ DotNet/EqualityDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// if (5 == "5") {
+//     Console.WriteLine("Values are equal!");
+// }
+// else {
+//     Console.WriteLine("Values are NOT equal.");
+// }
+
+if (100 == 100.0) {
+    Console.WriteLine("Values are equal!");
+}
+else {
+    Console.WriteLine("Values are NOT equal.");
+}
