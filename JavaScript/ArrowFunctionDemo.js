e24dcf3507fd9cd582fd82a41e23d000e3002db9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-15 11:43:45 +0200

    Changed the functions to be defined as arrow functions.

--- JavaScript/ArrowFunctionDemo.js
+++ JavaScript/ArrowFunctionDemo.js
@@ -1,16 +1,29 @@
-function add(a, b) {
-    return a + b;
-}
+const add = (a, b) => a + b;
 
 const myObject = {
     value: 10,
-    multiply: function (n) {
-        setTimeout(function () {
-            console.log(this.value * n);
-        }.bind(this), 500);
+    multiply: (n) => {
+        setTimeout(() => console.log(myObject.value * n), 500);
     }
 };
 
+console.log("Calling arrow functions:")
 const sum = add(1, 2);
 console.log(sum);
 myObject.multiply(50);
+
+// wait for a keypress to exit
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+process.stdin.setRawMode(true);
+
+process.stdin.on("data", (key) => {
+    console.log("Exiting application.");
+    process.exit();
+});
+
+process.on("exit", () => {
+  process.stdin.setRawMode(false);
+  process.stdin.pause();
+});
+console.log("Press any key to exit...");
