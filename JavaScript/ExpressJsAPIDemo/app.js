3cf1ea78cbc1a0e93c69f4be4cce87bc529259f9
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-16 11:18:37 +0200

    Added Express.js API endpoints for date, sum calculation and CSV file parsing in two different implementations.

--- JavaScript/ExpressJsAPIDemo/app.js
+++ JavaScript/ExpressJsAPIDemo/app.js
@@ -1,11 +1,71 @@
 const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
+});
+
+app.get("/date", (req, res) => {
+    res.send(new Date().toString());
+});
+
+// http://localhost:3000/sum?num1=100&num2=250
+app.get("/sum", (req, res) => {
+    const num1 = req.query.num1;
+    const num2 = req.query.num2;
+    const sum = parseInt(num1) + parseInt(num2);
+    res.send(sum.toString());
+});
+
+app.get("/persons1", (req, res) => {
+    fs.readFile("Persons.csv").then((data) => {
+        const persons = data.toString();
+        const personToReturn = [];
+        // console.log(persons);
+
+        const lines = persons.split("\r\n");
+        console.log(lines.length);
+        for (let index = 1; index < lines.length; index++) {
+            const line = lines[index];
+            const parts = line.split(",");
+            // console.log(parts[0]);
+
+            personToReturn.push({
+                personName: parts[0],
+                yearOfBirth: parts[1],
+                country: parts[2]
+            });
+        }
+
+        res.json(personToReturn);
+    });
+});
+
+app.get("/persons2", (req, res) => {
+    const personsData = [];
+
+    reader = fs.createReadStream("Persons.csv")
+        .pipe(csv({ headers: true, separator: ',' }))
+        .on('data', (row) => {
+            // console.log(row);
+            let person = {
+                personName: row["_0"],
+                yearOfBirth: row["_1"],
+                country: row["_2"]
+            };
+
+            if (person.personName != "PersonName") {
+                personsData.push(person);
+            }
+        })
+        .on('end', () => {
+            res.json(personsData);
+        });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}/`);
+    console.log(`Example app listening on http://localhost:${port}/`);
 });
