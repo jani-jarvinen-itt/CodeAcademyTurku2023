140f2dd6f927da99fc1068961675624086212758
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-16 13:31:39 +0200

    Converted JavaScript functions to TypeScript annotated versions.

--- TypeScript/ConversionDemo.ts
+++ TypeScript/ConversionDemo.ts
@@ -1,20 +1,27 @@
 // Function to add two numbers
-function addNumbers(a, b) {
+function addNumbers(a: number, b: number): number {
     return a + b;
 }
 
 // Function to greet a user
-function greet(name) {
+function greet(name: string): string {
     return `Hello, ${name}!`;
 }
 
 // Function to find the max number in an array
-function findMax(arr) {
+function findMax(arr: number[]): number {
     return Math.max(...arr);
 }
 
+interface Person {
+    name: string,
+    age: number
+}
+
 // Function to update a property in an object
-function updateObjectProperty(obj, property, value) {
+function updateObjectProperty(obj: Person, property: string,
+    value: string | number | boolean): Person {
+
     obj[property] = value;
     return obj;
 }
@@ -24,3 +31,18 @@ console.log(addNumbers(10, 5)); // 15
 console.log(greet("Alice")); // "Hello, Alice!"
 console.log(findMax([1, 3, 2, 5, 4])); // 5
 console.log(updateObjectProperty({ name: "Bob", age: 30 }, "age", 31)); // { name: "Bob", age: 31 }
+
+/*
+interface Person {
+    name: string,
+    age: number,
+    country: string,
+    employed?: boolean
+}
+
+let myPerson: Person = {
+    name: "John Doe",
+    age: 32,
+    country: "Finland"
+};
+*/
