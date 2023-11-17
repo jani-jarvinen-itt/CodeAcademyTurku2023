2e9fed560b1bd6ca2b809294374b193a57112633
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-17 11:49:09 +0200

    Added four basic methods for the Car class.

--- JavaScript/ObjectOrientedDemo.js
+++ JavaScript/ObjectOrientedDemo.js
@@ -6,6 +6,25 @@ class Car {
         this.make = "";
         this.model = "";
         this.topSpeed = 0;
+
+        this.engineRunning = false;
+        this.currentSpeed = 0;
+    }
+
+    startEngine() {
+        this.engineRunning = true;
+    }
+
+    stopEngine() {
+        this.engineRunning = false;
+    }
+    
+    accelerate() {
+        this.currentSpeed += 10;
+    }
+    
+    decelerate() {
+        this.currentSpeed -= 10;
     }
 }
 
@@ -16,6 +35,8 @@ myCar.make = "Toyota";
 myCar.model = "Corolla";
 myCar.topSpeed = 180;
 myCar.someProperty = "ABC"; // new property!
+myCar.startEngine();
+myCar.accelerate();
 console.log(myCar);
 
 const friendsCar = new Car();
