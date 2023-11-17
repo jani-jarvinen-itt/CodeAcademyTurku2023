39a4df06e70d05aec05ee32a823b886d59879b66
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-11-17 15:30:47 +0200

    Started to develop the very first Next.js application.

---
+++ TypeScript/ObjectOrientedDemo.js
@@ -0,0 +1,49 @@
+// definition of the class
+class Car {
+    // public properties
+    make;
+    model;
+    topSpeed;
+    // private fields
+    #engineRunning = false;
+    #currentSpeed = 0;
+    constructor() {
+        // initialize properties
+        this.make = "";
+        this.model = "";
+        this.topSpeed = 0;
+    }
+    startEngine() {
+        this.#engineRunning = true;
+    }
+    stopEngine() {
+        this.#engineRunning = false;
+    }
+    accelerate() {
+        if (this.#engineRunning) {
+            this.#currentSpeed += 10;
+            if (this.#currentSpeed > this.topSpeed) {
+                this.#currentSpeed = this.topSpeed;
+            }
+        }
+    }
+    decelerate() {
+        this.#currentSpeed -= 10;
+        if (this.#currentSpeed < 0) {
+            this.#currentSpeed = 0;
+        }
+    }
+}
+// the usage of the class
+const myCar = new Car();
+myCar.make = "Toyota";
+myCar.model = "Corolla";
+myCar.topSpeed = 180;
+// myCar.someProperty = "ABC"; // new property!
+myCar.startEngine();
+myCar.accelerate();
+// private fields cannot be set from the outside
+// myCar.#currentSpeed = -500;
+// myCar.#engineRunning = "ABCD";
+console.log(myCar);
+const friendsCar = new Car();
