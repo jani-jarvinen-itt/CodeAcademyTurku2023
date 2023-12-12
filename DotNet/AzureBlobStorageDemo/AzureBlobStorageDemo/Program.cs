ce3b1563beaffcdb1dfdfb06ddc05f44944c0fc7
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-12-12 12:01:11 +0200

    Added simple code to create a new container in the blob storage.

--- DotNet/AzureBlobStorageDemo/AzureBlobStorageDemo/Program.cs
+++ DotNet/AzureBlobStorageDemo/AzureBlobStorageDemo/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Storage.Blobs;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=codeacademy2023;AccountKey=gJZJv3NZLAkMyX6P41JOhh7EEj+zML9PhimAruDdipcGD00ddmNEkvvmYNEAre2ZJ2gHKiDb4LMc+AStioOC0g==;EndpointSuffix=core.windows.net";
+string containerName = "jani";
+
+Console.WriteLine("Starting to access Azure blob storage...");
+BlobContainerClient container = new(connectionString, containerName);
+container.Create();
+Console.WriteLine("Blob container created!");
